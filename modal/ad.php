b1fd3c47429b90dc5a780bb2588fc4d5612e58dd
reg <reg8740223@gmail.com>
2024-12-02 16:27:03 +0800

    乙級

---
+++ modal/ad.php
@@ -0,0 +1,24 @@
+<h3 class="cent">新增動態文字廣告</h3>
+<hr>
+<form action="api/insert_title.php" method="post" enctype="multipart/form-date">
+
+    <table>
+        <tr>
+            <td>標動態文字廣告:</td>
+            <td><input type="file" name="img" id="img"></td>
+        </tr>
+
+    </table>
+    <div class="cent">
+
+        <input type="submit" value="新增">
+        <input type="reset" value="重置">
+
+    </div>
+
+
+
+
+
+
+</form>
