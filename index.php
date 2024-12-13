f945427d3a67cf3fe8b8b85b72db1c25dd71660f
reg <reg8740223@gmail.com>
2024-12-13 13:45:40 +0800

    在標題圖片加上替代文字顯示

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
     </div>
 
     <div id="main">
-        <a title="" href="index.php">
+        <a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php">
 
             <div class="ti"
                 style="background:url('./upload/<?=$Title->find(['sh'=>1])['img'];?>'); background-size:cover;"></div>
